09ecf604ac8b034c23e2d05167697d185bb09808
WillTarte <William.tarte6@gmail.com>
2020-11-04 15:14:13 -0500

    Did some more work, fixed some stuff

--- xpuzzle.py
+++ xpuzzle.py
@@ -1,105 +1,183 @@
-import numpy
+import numpy as np
 from typing import List, Dict, Tuple, Type
 from abc import ABC, abstractmethod
 
+
 class XPuzzle:
     """
-    Represents some state of the XPuzzle
+    Represents some state s of the XPuzzle
     """
-    def __init__(self, current_state: List[int], size: Tuple[int, int] = (2,4)):
+
+    def __init__(self, current_state: List[List[int]], size: Tuple[int, int] = (2, 4)):
         """
-        Constructs an XPuzzle state from a given list if integers.
+        Constructs an XPuzzle state from a given 2d array.
         """
-        assert len(current_state) == size[0] * size[1], "Invalid size. Array was {0} tiles. Size was ({1}, {2})".format(len(current_state), size[0], size[1])
-        
-        self.state: List[int] = current_state
-        self.valid_moves: List[Type[Move]] = []
+        assert set(np.ravel(current_state)) == set(
+            range(0, size[0] * size[1])
+        ), "Invalid puzzle configuration {0}".format(current_state)
+
+        self.state = np.array(current_state)
+        self.valid_moves: List[Tuple[Type[Move], "XPuzzle"]] = []
         self.size: Tuple[int, int] = size
-    
+
     @classmethod
-    def from_file(cls, filename: str, size: Tuple[int, int] = (2,4)) -> List['XPuzzle']:
+    def from_file(
+        cls, filename: str, size: Tuple[int, int] = (2, 4)
+    ) -> List["XPuzzle"]:
+        """
+        Returns an array of XPuzzles based on a file's content where each line is one puzzle configuration.
+        Each puzzle is assumed to have the same size
+        """
         acc: List[XPuzzle] = []
+
         with open(filename, "r") as f:
+
             for line in f.readlines():
                 if line == "":
                     break
+
                 puzzle_data: List[int] = []
                 for char in (line.strip("\n")).replace(" ", ""):
                     puzzle_data.append(int(char))
-                acc.append(XPuzzle(puzzle_data, size))
+
+                state_data: List[List[int]] = []
+                for row_idx in range(0, size[0]):
+                    state_data.append([])
+                    for col_idx in range(0, size[1]):
+                        state_data[row_idx].append(
+                            puzzle_data[row_idx * size[1] + col_idx]
+                        )
+
+                acc.append(XPuzzle(state_data, size))
+
         return acc
 
-
     @classmethod
-    def from_array(cls, array: List[int], size: Tuple[int, int] = (2,4)) -> 'XPuzzle':
-        return XPuzzle(array, size)
+    def from_array(cls, array: List[int], size: Tuple[int, int] = (2, 4)) -> "XPuzzle":
+        """
+        Returns a XPuzzle from a given flat array with given size.
+        """
 
-    def is_goal_state(self)-> bool:
+        assert (
+            len(array) == size[0] * size[1]
+        ), "Invalid size. Array has {0} tiles. Size was {1}".format(len(array), size)
+        assert (
+            array.count(0) == 1
+        ), "Invalid board configuration. Too many/not enough '0's"
+
+        state_data: List[List[int]] = []
+        for row_idx in range(0, size[0]):
+            state_data.append([])
+            for col_idx in range(0, size[1]):
+                state_data[row_idx].append(array[row_idx * size[1] + col_idx])
+
+        return XPuzzle(state_data, size)
+
+    def find_valid_moves(self):
+        """
+        Populates this state's 'valid_moves' array with the moves we can take on the current state.
+        """
+        blank_tile_idx = numpy.where(self.state == 0)
+        blank_tile_idx = (blank_tile_idx[0][0], blank_tile_idx[1][0])
+
+        # horizontal moves
+
+    def is_goal_state(self) -> bool:
+        """
+        Returns True if this puzzle state is within 1 of 2 goal configurations.
+        """
         goal_1 = True
         goal_2 = True
 
-        for i in range (0, len(self.state) - 1):
-            if self.state[i+1] < self.state[i]:
+        for row in self.state:
+            if list(filter(lambda x: x != 0, row)) != sorted(
+                list(filter(lambda x: x != 0, row))
+            ):
                 goal_1 = False
                 break
-        
-        for col_idx in range(0, self.size[1]):
-            for row_idx in range (0, self.size[0] - 1):
-               if self.state[row_idx * self.size[1] + col_idx] < self.state[(row_idx+1) * self.size[1] + col_idx]:
-                   goal_2 = False
+
+        if not goal_1:
+            for col_idx in range(0, self.size[1]):
+                for row_idx in range(0, self.size[0] - 1):
+                    if (
+                        self.state[row_idx + 1][col_idx]
+                        != self.state[row_idx][col_idx] + 1
+                    ):
+                        goal_2 = False
+                        break
 
         return goal_1 or goal_2
 
     def __repr__(self) -> str:
         acc_str: str = ""
-        for row_idx in range(0, self.size[0]):
-            for col_idx in range(0, self.size[1]):
-                if col_idx == 0:
-                    acc_str += str(self.state[row_idx * self.size[1] + col_idx])
-                else:
-                    acc_str += " " + str(self.state[row_idx * self.size[1] + col_idx])
+        for row in self.state:
+            for tile in row:
+                acc_str += str(tile) + " "
+
+            acc_str = acc_str.strip()
             acc_str += "\n"
-        return acc_str.strip()
+
+        return acc_str.strip("\n")
 
     def __eq__(self, other) -> bool:
-        return self.state == other.state
-
+        return np.array_equal(self.state, other.state)
 
 
 class Move(ABC):
     @abstractmethod
-    def __init__(self):
-        pass
-    
-    @abstractmethod
+    def __init__(self, idx1: Tuple[int, int], idx2: Tuple[int, int]):
+        self.idx1 = idx1
+        self.idx2 = idx2
+
     def execute(self, puzzle: XPuzzle):
-        pass
+        (
+            puzzle.state[self.idx1[0]][self.idx1[1]],
+            puzzle.state[self.idx2[0]][self.idx2[1]],
+        ) = (
+            puzzle.state[self.idx2[0]][self.idx2[1]],
+            puzzle.state[self.idx1[0]][self.idx1[1]],
+        )
+
 
 class VerticalMove(Move):
-    def __init__(self, idx1: int, idx2: int):
+    def __init__(self, idx1: Tuple[int, int], idx2: Tuple[int, int]):
+        """
+        Constructs a vertical move for the board tiles at idx1 and idx2.
+        """
         self.name: str = "Vertical Move"
         self.cost: int = 1
-        self.idx1: int = idx1
-        self.idx2: int = idx2
+        self.idx1 = idx1
+        self.idx2 = idx2
+
 
 class HorizontalMove(Move):
-    def __init__(self, idx1: int, idx2: int):
+    def __init__(self, idx1: Tuple[int, int], idx2: Tuple[int, int]):
+        """
+        Constructs a horizontal move for the board tiles at idx1 and idx2.
+        """
         self.name: str = "Horizontal Move"
         self.cost: int = 1
-        self.idx1: int = idx1
-        self.idx2: int = idx2
+        self.idx1 = idx1
+        self.idx2 = idx2
+
 
 class WrappingMove(Move):
-    def __init__(self, idx1: int, idx2: int):
+    def __init__(self, idx1: Tuple[int, int], idx2: Tuple[int, int]):
+        """
+        Constructs a wrapping move for the board tiles at idx1 and idx2.
+        """
         self.name: str = "Wrapping Move"
         self.cost: int = 2
-        self.idx1: int = idx1
-        self.idx2: int = idx2
+        self.idx1 = idx1
+        self.idx2 = idx2
+
 
 class DiagonalMove(Move):
-    def __init(self, idx1: int, idx2: int):
+    def __init__(self, idx1: Tuple[int, int], idx2: Tuple[int, int]):
+        """
+        Constructs a diagonal move for the board tiles at idx1 and idx2.
+        """
         self.name: str = "Diagonal Move"
         self.cost: int = 3
-        self.idx1: int = idx1
-        self.idx2: int = idx2
-
+        self.idx1 = idx1
+        self.idx2 = idx2
--- xpuzzle_tests.py
+++ xpuzzle_tests.py
@@ -1,14 +1,18 @@
 import unittest
 from xpuzzle import XPuzzle
 from typing import List, Tuple, Dict
+import numpy as np
+
 
 class TestXPuzzle(unittest.TestCase):
-
     def test_from_array_should_pass(self):
         data_array: List[int] = [3, 0, 1, 4, 2, 6, 5, 7]
         to_test: XPuzzle = XPuzzle.from_array(data_array)
-        self.assertEqual(data_array, to_test.state, "Should be equivalent")
-        self.assertEqual((2,4), to_test.size, "Should be equivalent")
+        self.assertTrue(
+            np.array_equal(np.array([[3, 0, 1, 4], [2, 6, 5, 7]]), to_test.state),
+            "Should be equivalent",
+        )
+        self.assertEqual((2, 4), to_test.size, "Should be equivalent")
 
     def test_from_array_wrong_size_should_fail(self):
         data_array1: List[int] = [3, 0, 1, 4, 2, 6, 5, 7]
@@ -22,41 +26,111 @@ class TestXPuzzle(unittest.TestCase):
         data_array3: List[int] = [3, 0, 1, 4, 2, 6]
         with self.assertRaises(AssertionError):
             XPuzzle.from_array(data_array3)
-    
+
     def test_repr_should_pass(self):
         data_array: List[int] = [3, 0, 1, 4, 2, 6, 5, 7]
         to_test: XPuzzle = XPuzzle.from_array(data_array)
         expected = "3 0 1 4\n2 6 5 7"
         result = to_test.__repr__()
         self.assertEqual(expected, result, "Should be the same")
-    
+
     def test_from_array_custom_size_should_pass(self):
-        data_array: List[int] = [3, 0, 1, 4, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-        puzzle_to_test1 = XPuzzle.from_array(data_array, (3,6))
-        puzzle_to_test2 = XPuzzle.from_array(data_array, (6,3))
+        data_array: List[int] = [
+            3,
+            0,
+            1,
+            4,
+            2,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+        ]
+        puzzle_to_test1 = XPuzzle.from_array(data_array, (3, 6))
+        puzzle_to_test2 = XPuzzle.from_array(data_array, (6, 3))
 
-        self.assertEqual(data_array, puzzle_to_test1.state, "Should be equivalent")
-        self.assertEqual((3,6), puzzle_to_test1.size, "Should be equivalent")
+        self.assertTrue(
+            np.array_equal(
+                np.array(
+                    [[3, 0, 1, 4, 2, 5], [6, 7, 8, 9, 10, 11], [12, 13, 14, 15, 16, 17]]
+                ),
+                puzzle_to_test1.state,
+            ),
+            "Should be True",
+        )
+        self.assertEqual((3, 6), puzzle_to_test1.size, "Should be equivalent")
 
-        self.assertEqual(data_array, puzzle_to_test2.state, "Should be equivalent")
-        self.assertEqual((6,3), puzzle_to_test2.size, "Should be equivalent")
+        self.assertTrue(
+            np.array_equal(
+                np.array(
+                    [
+                        [3, 0, 1],
+                        [4, 2, 5],
+                        [6, 7, 8],
+                        [9, 10, 11],
+                        [12, 13, 14],
+                        [15, 16, 17],
+                    ]
+                ),
+                puzzle_to_test2.state,
+            ),
+            "Should be True",
+        )
+        self.assertEqual((6, 3), puzzle_to_test2.size, "Should be equivalent")
 
     def test_from_file_should_pass(self):
         puzzles_to_test: List[XPuzzle] = XPuzzle.from_file(r"./samplePuzzles.txt")
 
         self.assertEqual(3, len(puzzles_to_test), "There should be 3 puzzles")
 
-        self.assertEqual([3, 0, 1, 4, 2, 6, 5, 7], puzzles_to_test[0].state, "Should be equivalent")
-        self.assertEqual((2,4), puzzles_to_test[0].size, "should be equivalent")
-        
-        self.assertEqual([6, 3, 4, 7, 1, 2, 5, 0], puzzles_to_test[1].state, "Should be equivalent")
-        self.assertEqual((2,4), puzzles_to_test[1].size, "should be equivalent")
+        self.assertTrue(
+            np.array_equal(
+                np.array([[3, 0, 1, 4], [2, 6, 5, 7]]), puzzles_to_test[0].state
+            ),
+            "Should be True",
+        )
+        self.assertEqual((2, 4), puzzles_to_test[0].size, "should be equivalent")
 
-        self.assertEqual([1, 0, 3, 6, 5, 2, 7, 4], puzzles_to_test[2].state, "Should be equivalent")
-        self.assertEqual((2,4), puzzles_to_test[2].size, "should be equivalent")
+        self.assertTrue(
+            np.array_equal(
+                np.array([[6, 3, 4, 7], [1, 2, 5, 0]]), puzzles_to_test[1].state
+            ),
+            "Should be True",
+        )
+        self.assertEqual((2, 4), puzzles_to_test[1].size, "should be equivalent")
 
+        self.assertTrue(
+            np.array_equal(
+                np.array([[1, 0, 3, 6], [5, 2, 7, 4]]), puzzles_to_test[2].state
+            ),
+            "Should be equivalent",
+        )
+        self.assertEqual((2, 4), puzzles_to_test[2].size, "should be equivalent")
 
+    def test_eq_should_pass(self):
+        puzzle1 = XPuzzle.from_array([3, 0, 1, 4, 2, 6, 5, 7])
+        puzzle2 = XPuzzle.from_array([3, 0, 1, 4, 2, 6, 5, 7])
+        self.assertTrue(puzzle1 == puzzle2)
+
+    def test_is_goal_state_should_pass(self):
+        puzzle1 = XPuzzle.from_array([1, 2, 3, 4, 5, 6, 7, 0])
+        puzzle2 = XPuzzle.from_array([1, 3, 5, 7, 2, 4, 6, 0])
+        self.assertTrue(puzzle1.is_goal_state())
+        self.assertTrue(puzzle2.is_goal_state())
 
 
 if __name__ == "__main__":
     unittest.main()
+
+    # data_array: List[int] = [3, 0, 1, 4, 2, 6, 5, 7]
+    # to_test: XPuzzle = XPuzzle.from_array(data_array)
+    # print(to_test)
