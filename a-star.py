bc714f769caae5cbe3c1be22b24bf014f15a14d4
WillTarte <William.tarte6@gmail.com>
2020-11-09 13:08:38 -0500

    A star algo runs without crash. Still need to check if it works as intended

--- a-star.py
+++ a-star.py
@@ -1,47 +1,50 @@
-from xpuzzle import XPuzzle
+from xpuzzle import XPuzzle, PrioritizedPuzzle, Move
 from heuristics import calcH0, calcH1, calcH2
 from queue import PriorityQueue
 import numpy as np
-from typing import List, Tuple, Dict, Callable, Set, Type
+from typing import List, Tuple, Dict, Callable, Set, Type, Optional
 
 def a_star(starting_puzzle: XPuzzle, h: Callable):
     """
-    A Star Informed search algorithm
+    A Star Informed search algorithm.
+    https://en.wikipedia.org/wiki/A*_search_algorithm#Description
+    https://www.redblobgames.com/pathfinding/a-star/implementation.html
     """
 
-    openSet: PriorityQueue[Tuple[int, XPuzzle]] = PriorityQueue()
-    openSet.put((0, starting_puzzle))
+    openSet: PriorityQueue[PrioritizedPuzzle] = PriorityQueue()
+    openSet.put(PrioritizedPuzzle(0, starting_puzzle))
 
     closedSet: Set[XPuzzle] = set()
 
-    cameFrom: Dict[XPuzzle, XPuzzle] = {}
+    cameFrom: Dict[XPuzzle, Optional[XPuzzle]] = {}
+    cameFrom[starting_puzzle] = None
 
     gScore: Dict[XPuzzle, int] = {}
     gScore[starting_puzzle] = 0
 
     fScore: Dict[XPuzzle, int] = {}
-    fScore[starting_puzzle] = h(starting_puzzle.state)
+    fScore[starting_puzzle] = min(h(starting_puzzle.state))
 
     while not openSet.empty():
 
         priority, current = openSet.get()
         if current.is_goal_state():
-            # TODO
-            pass
+            break
 
         if len(current.valid_moves) == 0:
             current.find_valid_moves()
         
         for move, neighbour in current.valid_moves:
-            temp_gscore = gScore[current] + move.cost
-            if temp_gscore < gScore[neighbour]:
+            new_cost: int = gScore[current] + move.cost
+            if neighbour not in gScore.keys() or new_cost < gScore[neighbour]:
                 cameFrom[neighbour] = current
-                gScore[neighbour] = temp_gscore
-                fScore[neighbour] = gScore[neighbour] + h(neighbour.state)
+                gScore[neighbour] = new_cost
+                fScore[neighbour] = gScore[neighbour] + min(h(neighbour.state))
                 
-                if (fScore[neighbour], neighbour) not in openSet.queue:
-                    openSet.put((fScore[neighbour], neighbour))
+                if PrioritizedPuzzle(fScore[neighbour], neighbour) not in openSet.queue:
+                    openSet.put(PrioritizedPuzzle(fScore[neighbour], neighbour))
 
+    return cameFrom, fScore
 
 if __name__ == "__main__":
 
